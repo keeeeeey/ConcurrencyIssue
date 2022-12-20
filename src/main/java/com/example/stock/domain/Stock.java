f751208c384bbd94214703937c68db0dd8772b36
keeeeeey <sseioul@naver.com>
2022-12-20 20:35:23 +0900

    [FEAT] synchronized을 이용한 동시성 이슈 해결

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,37 @@
+package com.example.stock.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Stock {
+    //id, productId, quantity
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if (this.quantity - quantity < 0) {
+            throw new RuntimeException("재고 수량 부족");
+        }
+
+        this.quantity -= quantity;
+    }
+
+}
